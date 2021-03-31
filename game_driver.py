8deed04a543f9b7559dbd10a2e1e48e0055509e5
John Foley <jfoley@yesware.com>
2021-03-30 23:26:32 -0400

    refactor

--- game_driver.py
+++
@@ -1,8 +0,0 @@
-from scrabbler.run_simulations import RunSimulations
-
-def main():
-    n = 1
-    RunSimulations.run(n)
-
-if __name__ == "__main__":
-    main()
--- scrabbler/run_simulations.py
+++
@@ -1,7 +0,0 @@
-from scrabbler.simulation import Simulation
-
-class RunSimulations:
-    @staticmethod
-    def run(n):
-        for i in range(n):
-            Simulation.simulate_game()
