aa77c61f039a0a3121f2ee50c86f8438f00d1774
John Foley <jfoley@yesware.com>
2021-04-18 14:15:07 -0400

    params for large training session

---
+++ dqn/epsilon.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import math
+
+start = 1.0
+end = 0.1
+decay = 5000
+steps = 27000
+
+thresholds = []
+
+for i in range(steps):
+    threshold = end + (start - end) * \
+        math.exp(-1 * i / decay)
+    thresholds.append(threshold)
+
+iters = [i for i in range(steps)]
+
+plt.plot(iters, thresholds)
+plt.show()
